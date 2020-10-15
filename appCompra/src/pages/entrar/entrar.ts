94a0e6be511affdb4c7234e2c37be2d7be93a748
bombeardor <jorge.pcpm@hotmail.com>
2020-10-14 21:11:15 -0300

    primeiro commit

---
+++ appCompra/src/pages/entrar/entrar.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { HomePage } from '../home/home';
+
+import { IUsuario } from '../../interfaces/IUsuario';
+
+import { UsuariosProvider } from '../../providers/usuarios/usuarios';
+
+/**
+ * Generated class for the EntrarPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-entrar',
+  templateUrl: 'entrar.html',
+})
+export class EntrarPage {
+  usuario:IUsuario = {email:'',password:''};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public usuariosProvider:UsuariosProvider) {
+  }
+
+  ionViewDidLoad() {
+
+  }
+
+  cancelar(){
+    this.navCtrl.setRoot(HomePage);
+  }
+
+  loginUsuario(){
+    this.usuariosProvider.loginUsuario(this.usuario).subscribe(res => {
+      console.log(res);
+    }, erro => {
+      console.log("Erro: " + erro.message);
+    });
+  }
+
+}
---
+++ appCompra/src/providers/usuarios/usuarios.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IUsuario } from '../../interfaces/IUsuario';
+/*
+  Generated class for the UsuariosProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UsuariosProvider {
+
+  url:string = 'http://localhost:3000/';
+  headers:any;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello UsuariosProvider Provider');
+    //this.headers = {"headers": {"authorization": "Bearer "}};
+
+  }
+
+  showUsuario(data:IUsuario){
+    return this.http.get<IUsuario>(this.url +'usuarios/'+data.id);
+  }
+
+  loginUsuario(data:IUsuario){
+    return this.http.get<IUsuario>(this.url +'usuarios/1');
+  }
+
+  addUsuario(data:IUsuario){
+    return this.http.post<IUsuario>(this.url +'usuarios',data);
+  }
+
+  editUsuario(data:IUsuario){
+    return this.http.put<IUsuario>(this.url +'usuarios/'+data.id,data);
+  }
+
+
+}
